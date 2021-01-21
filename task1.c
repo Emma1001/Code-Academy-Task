67ea079f8909efe114d71cab7ae6ad07f807df89
Desislava Velkova <desi_8459@abv.bg>
2021-01-21 14:43:52 +0200

    Add if-else

--- task1.c
+++ task1.c
@@ -6,6 +6,19 @@ int main() {
         printf("Enter your height: ");
         scanf("%f", &height);
     } while (height <= 0);
-    
+
+    if (height <= 145) {
+        printf("Small height\n");
+    }
+    else if(height >= 146 && height <= 180) {
+        printf("Average height\n");
+    }
+    else if(height >= 181 && height <= 205) {
+        printf("High height\n");
+    }
+    else {
+        printf("Extremely high height\n");
+    }
+
     return 0;
 }
